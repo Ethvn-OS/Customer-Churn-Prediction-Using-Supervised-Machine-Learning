c14b164d81a1eca1369346d256d5878db9f93bb2
Ethan Andre <andreverallo14@gmail.com>
2026-06-02 16:14:53 +0800

    feat: added conditional probability to select predictors
    - Also added comments to get insights on such predictors.

--- SupervisedMLCode.R
+++ SupervisedMLCode.R
@@ -2,6 +2,7 @@ library(knitr)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(psych)
 
 # Importing dataset
 init_data <- read.csv('C:/Data Analysis/datasets/customer_churn.csv')
@@ -10,6 +11,8 @@ head(init_data)
 dim(init_data)
 summary(init_data)
 str(init_data)
+typeof(init_data)
+class(init_data)
 
 # The TotalCharges column has 11 missing values
 colSums(is.na(init_data))
@@ -31,6 +34,8 @@ filter(init_data, is.na(TotalCharges))
 init_data <- filter(init_data, !is.na(TotalCharges))
 
 # Checking class imbalance
+# In this dataset, there are generally more people who DON'T churn...
+# There is imbalance in the dataset
 table(init_data$Churn)
 prop.table(table(init_data$Churn))
 
@@ -70,3 +75,46 @@ ggplot(init_data, aes(x = PaymentMethod, fill = PaymentMethod)) +
     y = "Count"
   )
   
+# Descriptive statistics for numerical variables
+result <- describe(init_data[, c("tenure", "MonthlyCharges", "TotalCharges")])
+kable(result, digits = 2, caption = "Descriptive Statistics of Churn Dataset")
+
+# Conditional Probabilities
+# Customers are less likely to churn if they have a two year or one year contract
+# This is in contrast to people who have a month-to-month contract
+prop.table(
+  table(init_data$Contract, init_data$Churn),
+  margin = 1
+)
+
+# Less likely to churn if naay partner
+prop.table(
+  table(init_data$Partner, init_data$Churn),
+  margin = 1
+)
+
+# If they have DSL or No internet service, they are less likely to churn
+prop.table(
+  table(init_data$InternetService, init_data$Churn),
+  margin = 1
+)
+
+# No internet service and has tech support: less likely to churn
+prop.table(
+  table(init_data$TechSupport, init_data$Churn),
+  margin = 1
+)
+
+# If not paperless billing, less likely to churn
+prop.table(
+  table(init_data$PaperlessBilling, init_data$Churn),
+  margin = 1
+)
+
+# Many customers use electronic check (according to the barplot)
+# but customers who prefer bank transfer, credit card, and mailed check 
+# are the ones who are less likely to churn.
+prop.table(
+  table(init_data$PaymentMethod, init_data$Churn),
+  margin = 1
+)
